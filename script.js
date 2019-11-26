94e0878c3812a2e416fb7f7bf85283906f1808ef
Isaiah Arroyo <isaiah.arroyo@centre.edu>
2019-11-26 02:32:20 -0500

    works

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ gdpPromise.then(
 function(data)
     {
         console.log("GDP",data)
+        setup(data);
     },
 function(err)
     {
@@ -27,7 +28,7 @@ var drawGraph = function(data)
         .each(horizonChart);
 }
 
-var screen = {width:400,height:500}
+var screen = {width:500,height:500}
 var margins = {top:10,right:50,bottom:50,left:50}
 
 var setup = function(array2D)
@@ -42,5 +43,52 @@ var setup = function(array2D)
     var width = screen.width - margins.left - margins.right;
     var height = screen.height - margins.top - margins.bottom;
     
-    var xScale
+    var xScale = d3.scaleLinear().domain([2000,2017]).range([0,width])
+    var yScale = d3.scaleLinear().domain([0,10]).range([height,0])
+    
+    var cScale = d3.scaleOrdinal(d3.schemeTableau10)
+    
+    var xAxis = d3.axisBottom(xScale)
+    var yAxis = d3.axisLeft(yScale)
+    d3.select("svg")
+        .append("g")
+        .classed("axis",true)
+    
+    d3.select(".axis")
+        .append("g")
+        .attr("id","xAxis")
+        .attr("transform","translate("+margins.left+","+(margins.top+height)+")")
+        .call(xAxis)
+    
+    d3.select(".axis")
+        .append("g")
+        .attr("id","yAxis")
+        .attr("transform","translate(25,"+margins.top+")")
+        .call(yAxis)
+    
+    drawArray(array2D,xScale,yScale,cScale)
+}
+
+var drawArray = function(array2D,xScale,yScale,cScale)
+{
+    var arrays = d3.select("#graph")
+        .selectAll("g")
+        .data(array2D)
+        .enter()
+        .append("g")
+        .attr("fill","none")
+        .attr("stroke",function(arr)
+             {
+                return cScale(arr.Region);
+             })
+        .attr("stroke-width",3)
+    
+    var lineGenerator = d3.line()
+        .x(function(num,index){return xScale(index)})
+        .y(function(num){return yScale(num)})
+        .curve(d3.curveNatural)
+    
+    arrays.append("path")
+        .datum(function(obj){return obj.arr})
+        .attr("d",lineGenerator)
 }
