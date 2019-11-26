a05720d53bc860bd2875e4d6e38839fbf674b0d8
Isaiah Arroyo <isaiah.arroyo@centre.edu>
2019-11-26 10:49:10 -0500

    still working

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-var gdpPromise = d3.csv("birth.csv")
+var gdpPromise = d3.csv("gdp.csv")
 
 gdpPromise.then(
 function(data)
     {
         console.log("GDP",data)
-        //setup(data);
+        setup(data);
     },
 function(err)
     {
         console.log(err);
     })
+
+var getChange = function(gdp)
+{
+    return gdp.CHANGE; 
+}
+
+var getData = function(data)
+{
+    return data.map(getChange);
+}
 
 var drawGraph = function(data)
 {
@@ -28,11 +38,12 @@ var drawGraph = function(data)
         .each(horizonChart);
 }
 
-var screen = {width:500,height:500}
+var screen = {width:1500,height:600}
 var margins = {top:10,right:50,bottom:50,left:50}
 
 var setup = function(array2D)
 {
+    console.log(array2D)
     d3.select("svg")
         .attr("width",screen.width)
         .attr("height",screen.height)
@@ -43,13 +54,14 @@ var setup = function(array2D)
     var width = screen.width - margins.left - margins.right;
     var height = screen.height - margins.top - margins.bottom;
     
-    var xScale = d3.scaleLinear().domain([0,40]).range([0,width])
-    var yScale = d3.scaleLinear().domain([0,6]).range([height,0])
+    var xScale = d3.scaleLinear().domain([1,290]).range([0,width])
+    var yScale = d3.scaleLinear().domain([-15,20]).range([height,0])
     
     //var cScale = d3.scaleOrdinal(d3.schemeTableau10)
     
     var xAxis = d3.axisBottom(xScale)
     var yAxis = d3.axisLeft(yScale)
+    
     d3.select("svg")
         .append("g")
         .classed("axis",true)
@@ -57,36 +69,62 @@ var setup = function(array2D)
     d3.select(".axis")
         .append("g")
         .attr("id","xAxis")
-        .attr("transform","translate("+margins.left+","+(margins.top+height)+")")
+        .attr("transform","translate("+(margins.left-5)+","+(margins.top+(height-231))+")")
         .call(xAxis)
     
     d3.select(".axis")
         .append("g")
         .attr("id","yAxis")
-        .attr("transform","translate(25,"+margins.top+")")
+        .attr("transform","translate(45,"+margins.top+")")
         .call(yAxis)
+    
+    d3.select("#tooltip")
+        .selectAll("circle")
+        .data(getData(array2D))
+        .enter()
+        .append("circle")
+        .on("mouseover",function(stat)
+            {
+                d3.select("#tooltip")
+                    .style("left",(d3.event.pageX+20)+"px")
+                    .style("top",(d3.event.pageY-25)+"px")
+                    .text("("+stat.index+","+stat.arr+")")
+                    .classed("hidden",false)
+            })
+        .on("mouseout",function()
+            {
+                d3.select("#tooltip")
+                    .classed("hidden",true)
+            })
     
     drawArray(array2D,xScale,yScale)
 }
 
 var drawArray = function(array2D,xScale,yScale)
-{
+{   
+    var everything = getData(array2D)
+    
     var arrays = d3.select("#graph")
         .selectAll("g")
-        .data(array2D)
+        .data(everything)
         .enter()
         .append("g")
         .attr("fill","none")
         .attr("stroke","black")
-        .attr("stroke-width",3)
-    
+        .attr("stroke-width",2)
+
     var lineGenerator = d3.line()
-        .x(function(num,index){return xScale(index)})
-        .y(function(num){return yScale(num)})
-        .curve(d3.curveLinear)
+        .x(function(num,index)
+        {
+            return xScale(index)
+        })
+        .y(function(num){
+            return yScale(num)
+        })
+        .curve(d3.curveNatural)
     
     arrays.append("path")
-        .datum(function(obj){return obj.arr})
+        .datum(everything)
         .attr("d",lineGenerator)
 }
 
@@ -94,4 +132,5 @@ var thing = [
         {graph:"GDP", arr:[1.5,3.7,3.0,2.0,-1.0,2.9,-0.1,4.7,3.2,1.7,0.5,0.5,3.6,0.5,3.2,3.2,-1.1,5.5,5.0,2.3,3.2,3.0,1.3,0.1,2.0,1.9,2.2,2.0,2.3,2.2,3.2,3.5,2.5,3.5,2.9,1.1,3.1,2.0,1.9]}
     ]
 
-setup(thing);
+//setup(thing);
+
